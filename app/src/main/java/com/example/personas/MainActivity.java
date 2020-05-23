62fc9af0135085aa43cce90117ddd1118e236cc0
Pedro Hernandez <pihebor@hotmail.com>
2020-05-23 15:10:16 -0500

    Interfaz principal terminada con redireccion

--- app/src/main/java/com/example/personas/MainActivity.java
+++ app/src/main/java/com/example/personas/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.personas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView lista_opciones;
     private String[] opciones;
     private ArrayAdapter<String> adapter;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
         opciones = getResources().getStringArray(R.array.opciones);
         adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, opciones);
         lista_opciones.setAdapter(adapter);
+        lista_opciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        intent = new Intent(MainActivity.this, CrearPersonas.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(MainActivity.this, ListarPersona.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
 }
