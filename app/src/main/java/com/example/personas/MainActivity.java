4619e9239d695be637408b2f15f04024a4048e28
Pedro Hernandez <pihebor@hotmail.com>
2020-05-23 14:48:25 -0500

    Intrfaz lista a falta de funcionalidad

--- app/src/main/java/com/example/personas/MainActivity.java
+++ app/src/main/java/com/example/personas/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView lista_opciones;
+    private String[] opciones;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lista_opciones = findViewById(R.id.lstOpciones);
+        opciones = getResources().getStringArray(R.array.opciones);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, opciones);
+        lista_opciones.setAdapter(adapter);
     }
 }
